a308b3824df739f3b3e693ccb3492334868e3d69
Sebastian Kałuża <73588307+TurboSebo@users.noreply.github.com>
2024-11-25 20:21:00 +0100

    Cybercentrum Init

---
+++ cybercentrum/src/main/java/com/sebastiank/cybercentrum/CybercentrumApplication.java
@@ -0,0 +1,13 @@
+package com.sebastiank.cybercentrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybercentrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybercentrumApplication.class, args);
+	}
+
+}
---
+++ cybercentrum/src/test/java/com/sebastiank/cybercentrum/CybercentrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastiank.cybercentrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybercentrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
